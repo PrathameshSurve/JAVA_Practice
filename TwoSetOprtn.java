c1a9db80a1ec09bcad0d03fb508ef42149a49c82
Prathamesh Sanjay Surve <prathamsurve101@gmail.com>
2023-09-21 12:23:32 +0530

    Corrected Set codes

--- TwoSetOprtn.java
+++ TwoSetOprtn.java
@@ -22,44 +22,47 @@ public class TwoSetOprtn{
         System.out.println("This is the set1: " + set1);
         System.out.println("This is the set2: "+ set2);
 
-        Set<Integer> union = new HashSet<>();
-        union.addAll(set1);
-        union.addAll(set2);
-        System.out.println("This is the Union: "+ union);
+        // Set<Integer> union = new HashSet<>();
+        // union.addAll(set1);
+        // union.addAll(set2);
+        // System.out.println("This is the Union: "+ union);
 
-        Set<Integer> intrsect = new HashSet<>();
-        for(Integer elmnt: set1){
-            for (Integer el : set2)
-           if(set1.contains(el)){
-            intrsect.add(el);
-           }
-        }
-        System.out.println("This is the Intersect: "+ intrsect);
+        // Set<Integer> intrsect = new HashSet<>();
+        // for(Integer elmnt: set1){
+        //     for (Integer el : set2)
+        //    if(set1.contains(el)){
+        //     intrsect.add(el);
+        //    }
+        // }
+        // System.out.println("This is the Intersect: "+ intrsect);
 
-        Set<Integer> differ = new HashSet<>();
-        for(Integer elmnt: set1){
-            for (Integer el : set2)
-           if(!(set1.contains(el))){
-            differ.add(el);
-           }
-        }
-        System.out.println("This is the Difference: "+ differ);
+        // Set<Integer> differ = new HashSet<>();
+        // for(Integer elmnt: set1){
+        //     for (Integer el : set2)
+        //    if(!(set1.contains(el))){
+        //     differ.add(el);
+        //    }
+        // }
+        // System.out.println("This is the Difference: "+ differ);
 
 
-// Union of set1 and set2
+
         Set<Integer> union2 = new HashSet<>(set1);
         union2.addAll(set2);
 
-        // Intersection of set1 and set2
+        
         Set<Integer> intersection2 = new HashSet<>(set1);
         intersection2.retainAll(set2);
 
-        // Difference between set1 and set2
-        Set<Integer> differ2 = new HashSet<>(set1);
+     
+        Set<Integer> differ2 = new HashSet<>(union2);
         differ2.removeAll(set2);
+        set2.removeAll(set1);
+        differ2.addAll(set2);
+        
 
         System.out.println("This is the correct Union: "+ union2);
-        System.out.println("This is the correct Difference: "+ intersection2);
+        System.out.println("This is the correct Intersection: "+ intersection2);
         System.out.println("This is the correct Difference: "+ differ2);
        
     }
