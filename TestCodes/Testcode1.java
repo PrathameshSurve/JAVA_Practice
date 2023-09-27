6f9472b9529041284b9ec4199da98597b00e0e71
Prathamesh Sanjay Surve <prathamsurve101@gmail.com>
2023-09-27 22:44:52 +0530

    Added imp codes, Testing

---
+++ TestCodes/Testcode1.java
@@ -0,0 +1,19 @@
+package TestCodes;
+
+public class Testcode1{
+
+
+    public static int main(int n) {
+        return n*n;   
+    }
+
+    public boolean Even(int e){
+        return e % 2 == 0;
+    }
+
+    public boolean Odd(int e){
+        return e % 2 != 0;
+    }
+    
+
+}
---
+++ Testing/Test1.java
@@ -0,0 +1,30 @@
+package Testing;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import TestCodes.Testcode1;
+
+public class Test1 {
+
+    Testcode1 code1 = new Testcode1();
+
+    @Test
+    public void test(){
+        int n = 2;
+        
+    }
+
+    @Test 
+    public void evenTest(){
+        int e =4;
+        Assert.assertTrue(code1.Even(e));
+    }
+
+    @Test 
+    public void oddTest(){
+        int e =5;
+        Assert.assertTrue(code1.Odd(e));
+    }
+    
+}
