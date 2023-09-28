ca3c30a6ec676d6ef0e1818135e2c651dda8bc2c
Prathamesh Sanjay Surve <prathamsurve101@gmail.com>
2023-09-28 21:40:43 +0530

    Added test codes

--- Testing/Test1.java
+++ Testing/Test1.java
@@ -10,21 +10,21 @@ public class Test1 {
     Testcode1 code1 = new Testcode1();
 
     @Test
-    public void test(){
+    public void test() {
         int n = 2;
-        
+
     }
 
-    @Test 
-    public void evenTest(){
-        int e =4;
+    @Test
+    public void evenTest() {
+        int e = 4;
         Assert.assertTrue(code1.Even(e));
     }
 
-    @Test 
-    public void oddTest(){
-        int e =5;
+    @Test
+    public void oddTest() {
+        int e = 5;
         Assert.assertTrue(code1.Odd(e));
     }
-    
+
 }
