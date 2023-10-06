1c053cdf0007600f725623cfce5c5e0c8eb4d819
Prathamesh Sanjay Surve <prathamsurve101@gmail.com>
2023-10-06 20:18:07 +0530

    Added POJO codes

--- LooseCoupling.java
+++ LooseCoupling.java
@@ -2,6 +2,7 @@ public class LooseCoupling {
 
     public static void main(String[] args) {
         Child obj = new Child();
+        obj.fun();
     }
 
 }
---
+++ POJOEx.java
@@ -0,0 +1,42 @@
+
+//POJO is Plain Object Java Object  (This is simple inheritanmce)
+public class POJOEx {
+    public static void main(String[] args) {
+
+    }
+
+    POJOEx() {
+
+    }
+
+}
+
+class Pojo {
+
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        name = this.name;
+    }
+
+}
+
+// public class Pratham implements serializable{
+// private int age;
+
+// public Harshal() {
+// }
+
+// public int getAge() {
+// return age;
+// }
+
+// public void setAge(int age) {
+// this.age = age;
+// }
+
+// }
